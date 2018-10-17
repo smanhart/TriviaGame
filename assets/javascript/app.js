6f30dcbee82e00238b624d507a992bb6c84a625a
Lucky Penny <41957862+smanhart@users.noreply.github.com>
2018-10-16 20:27:37 -0600

    basic html, working on pseudocode

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+//var array to hold questions
+    //each question object w/ array of answers?
+//display first question on click event
+    //timed
+    //question in one div
+    //each answer put in their own div
+//get input from user guess
+    //listen for click 
+//display if guess is right or wrong - for a set time
+//move to next question
+//start timer over
+//when finished, show total scores
+//reset counters and timer and questions
